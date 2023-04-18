69614bffac922a9ce2078474b97795c34c5fda9c
Eric Chong <echong@redhat.com>
2023-04-18 15:20:43 -0400

    update json output

--- inventory.test.sh
+++ inventory.test.sh
@@ -2,15 +2,19 @@
 
 INV="{
 \"all\": {
-  \"hosts\": [
-     \"node1\",
-     \"node2\",
-     \"node3\"
-   ],
-  \"vars\": {
-     \"my_var\": \"123\",
-     \"FOREMAN_USERNAME\": \"$FOREMAN_USERNAME\"
+ \"children\": {
+   \"dummy_hosts\": {
+    \"hosts\": {
+      \"node1\": \"\",
+      \"node2\": \"\",
+      \"node3\": \"\"
+     },
+    \"vars\": {
+       \"my_var\": \"123\",
+       \"FOREMAN_USERNAME\": \"$FOREMAN_USERNAME\"
+    }
   }
+ }
 }
 }"
 
